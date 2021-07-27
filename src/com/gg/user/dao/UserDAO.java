6fd3cbc3d88cd7722dcb1d305b4ac03e94dd30b8
jaehyunjo <joh1160@naver.com>
2021-07-27 18:08:05 +0900

    joinForm에서 중복체크 및 회원가입 작성중(1)

--- src/com/gg/user/dao/UserDAO.java
+++ src/com/gg/user/dao/UserDAO.java
@@ -1,9 +1,46 @@
 package com.gg.user.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
 public class UserDAO {
 
+	public Connection conn = null;
+	public PreparedStatement ps = null;
+	public ResultSet rs = null;
+	String sql = "";
+	
 	public UserDAO() {
-		// TODO Auto-generated constructor stub
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/Oracle");
+			conn = ds.getConnection();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+	
+	
+	public void resClose() { 
+		try {
+			if (rs!=null && !rs.isClosed()) { rs.close();}
+			if (conn !=null && !conn.isClosed()) {conn.close();}
+			if (ps !=null && !ps.isClosed()) {ps.close();}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 
 }
--- src/com/gg/user/service/UserService.java
+++ src/com/gg/user/service/UserService.java
@@ -1,13 +1,40 @@
 package com.gg.user.service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class UserService {
 
 	HttpServletRequest req = null;
+	HttpServletResponse resp = null;
+	boolean success;
 	
-	public UserService(HttpServletRequest req) {
+	
+	
+	public UserService(HttpServletRequest req, HttpServletResponse resp) {
 		this.req = req;
+		this.resp = resp;
 	}
 
+	public boolean join() {
+		success = false;
+		System.out.println("회원 가입 요청 확인");
+		String id = req.getParameter("id");
+		String pw = req.getParameter("pw");
+		String name = req.getParameter("name");
+		String nname = req.getParameter("nname");
+		String age = req.getParameter("age");
+		String email = req.getParameter("email");
+		String mail = req.getParameter("mail");
+		String detailAddr = req.getParameter("detailAddr");
+		System.out.println("들어온 인자 값 확인!");
+		System.out.println(id+"/"+pw+"/"+name+"/"+nname+"/"+age+"/"+email+"/"+mail+"/"+detailAddr);
+		
+		return success;
+	}
+
+	
+	
+	
+	
 }
