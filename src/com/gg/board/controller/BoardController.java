9f6c4d7b8c69e73551ea7e14f42a94c6ed124551
hyesuelee <lhs75391@naver.com>
2021-07-29 17:35:22 +0900

    글작성....5 (판매글 작성 진행중...)

--- src/com/gg/board/controller/BoardController.java
+++ src/com/gg/board/controller/BoardController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.gg.board.service.BoardService;
 import com.gg.dto.GGDto;
 
-@WebServlet({"/salesDetail","/loveMinus","/lovePlus","/loveMinus2","/lovePlus2","/buyRequest","/buyRequestCancel"})
+@WebServlet({"/salesDetail","/loveMinus","/lovePlus","/loveMinus2","/lovePlus2","/buyRequest","/buyRequestCancel","/sold","/writeForm","/writeSale","/writeTrade","/writeCommunity"})
 public class BoardController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
