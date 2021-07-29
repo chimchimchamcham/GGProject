3fab66480e538438da67f86c6c977ef71561bf7d
JeongSeulgi <emily1770@naver.com>
2021-07-29 09:45:28 +0900

    충전팝업, 마이페이지 팝업 이름 변경, 포인트 컨트롤러

--- src/com/gg/user/controller/PointController.java
+++ src/com/gg/user/controller/PointController.java
@@ -2,13 +2,14 @@ package com.gg.user.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet
+@WebServlet({"/charge","/test"})
 public class PointController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -25,6 +26,24 @@ public class PointController extends HttpServlet {
 
 	private void dual(HttpServletRequest req, HttpServletResponse resp) {
 		
+		String uri = req.getRequestURI();
+		String ctx = req.getContextPath();
+		String addr = uri.substring(ctx.length());
+		System.out.println("addr : "+addr);
+		
+		RequestDispatcher dis = null;
+		String page = "";
+		String msg = "";
+		
+		switch (addr) {
+		case "/charge":
+			System.out.println("포인트 충전 요청");
+			int chargePoint = Integer.parseInt(req.getParameter("chargePoint"));
+			System.out.println("충전 요청 금액 : "+ chargePoint);
+			
+			break;
+
+		}
 		
 	}
 }
