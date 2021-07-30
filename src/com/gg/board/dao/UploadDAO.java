eea36aa1359eb3306bc38849787340d1cf5e3630
hyesuelee <lhs75391@naver.com>
2021-07-30 17:30:03 +0900

    사진 업로드 진행중.... 죽고 싶음.....

--- src/com/gg/board/dao/UploadDAO.java
+++ src/com/gg/board/dao/UploadDAO.java
@@ -1,9 +1,66 @@
 package com.gg.board.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.gg.dto.GGDto;
+
 public class UploadDAO {
+	
+	public Connection conn = null;
+	public ResultSet rs = null;
+	public PreparedStatement ps = null;
+
 
 	public UploadDAO() {
-		// TODO Auto-generated constructor stub
+		Context ctx;
+		try {
+			ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/Oracle");
+			conn = ds.getConnection();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void resClose() {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+			if (rs != null && !rs.isClosed()) {
+				rs.close();
+			}
+			if (ps != null && !ps.isClosed()) {
+				ps.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void PhotoUpload(GGDto dto) {
+		String sql = "INSERT INTO Img VALUES(?,?)";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, dto.getP_no());
+			ps.setString(2, dto.getI_newName());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
--- src/com/gg/board/service/UploadService.java
+++ src/com/gg/board/service/UploadService.java
@@ -18,7 +18,7 @@ public class UploadService {
 	}
 
 	public GGDto PhotoUpload() {
-		String savePath = "C:/img/";// 1.저장경로 저장
+		String savePath = "C:/photo/";// 1.저장경로 저장
 		int maxSize = 10 * 1024 * 1024;// 2.용량제한(10MB)
 		GGDto dto = null;
 
@@ -33,7 +33,27 @@ public class UploadService {
 			
 			MultipartRequest multi = new MultipartRequest(req, savePath, maxSize, "UTF-8",
 					new DefaultFileRenamePolicy());
-
+			
+			dto = new GGDto();
+			
+			String p_no = multi.getParameter("p_no");
+			if (p_no != null) {
+				System.out.println("p_no : " + p_no);
+				dto.setP_no(Integer.parseInt(p_no));
+			}
+			
+			String oriFileName = multi.getFilesystemName("imgFile");
+			System.out.println("oriFileName : "+oriFileName);
+			if(oriFileName != null) {
+				String ext = oriFileName.substring(oriFileName.lastIndexOf("."));
+				String i_newName = System.currentTimeMillis() + ext;
+				
+				File oldName = new File(savePath + oriFileName);
+				File newName = new File(savePath + i_newName);
+				oldName.renameTo(newName);
+				dto.setI_newName(i_newName);
+				
+			}
 			
 		} catch (IOException e) {
 
@@ -43,4 +63,5 @@ public class UploadService {
 		return dto;
 	}
 
+	
 }
