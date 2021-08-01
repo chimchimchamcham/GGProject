9ec39f8090d0caf574e3dd047e25eded778721f0
jaehyunjo <joh1160@naver.com>
2021-08-01 14:13:50 +0900

    마이페이지 -> 포인트 내역 수정, 포인트 충전 팝업 -> 포인트 내역 수정 SQL 변경

--- src/com/gg/user/controller/PointController.java
+++ src/com/gg/user/controller/PointController.java
@@ -1,6 +1,7 @@
 package com.gg.user.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,41 +10,61 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet({"/charge","/test"})
+import com.gg.user.service.PointService;
+import com.google.gson.Gson;
+
+@WebServlet({ "/charge", "/test", "/pointPop" })
 public class PointController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		dual(req,resp);
+		dual(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		dual(req,resp);
+		dual(req, resp);
 	}
 
-	private void dual(HttpServletRequest req, HttpServletResponse resp) {
-		
+	private void dual(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 		String uri = req.getRequestURI();
 		String ctx = req.getContextPath();
 		String addr = uri.substring(ctx.length());
-		System.out.println("addr : "+addr);
-		
+		System.out.println("addr : " + addr);
+
 		RequestDispatcher dis = null;
 		String page = "";
 		String msg = "";
-		
+
 		switch (addr) {
+
+		case "/pointPop":
+			System.out.println("포인트 충전 팝업 진입 완료");
+			boolean success = false;
+			PointService service = new PointService(req);
+			int myPoint = service.pointPop();
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			if(myPoint > - 1000000) { // 포인트가 음수가 될 순 없다.
+				success = true;
+			}
+			map.put("success", success);
+			map.put("myPoint", myPoint);
+
+			resp.getWriter().println(new Gson().toJson(map));
+			break;
+			
+			
 		case "/charge":
 			System.out.println("포인트 충전 요청");
 			int chargePoint = Integer.parseInt(req.getParameter("chargePoint"));
-			System.out.println("충전 요청 금액 : "+ chargePoint);
-			
+			System.out.println("충전 요청 금액 : " + chargePoint);
+
 			break;
 
 		}
-		
+
 	}
 }
--- src/com/gg/user/service/PointService.java
+++ src/com/gg/user/service/PointService.java
@@ -1,6 +1,10 @@
 package com.gg.user.service;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
+
+import com.gg.user.dao.PointDAO;
 
 public class PointService {
 	
@@ -9,5 +13,27 @@ public class PointService {
 	public PointService(HttpServletRequest req) {
 		this.req = req;
 	}
+
+	public int pointPop() {
+		System.out.println("팝업 정보 서비스 진입");
+		int myPoint = 0;
+		String id = req.getParameter("id");
+		System.out.println("아이디 : " + id);
+		PointDAO dao = new PointDAO();
+		try {
+			myPoint = dao.pointPop(id);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			dao.resClose();
+		}
+		
+		
+		return myPoint;
+	}
+	
+	
+	
 
 }
