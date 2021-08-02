f5aa30a72cbc537926c65ab1dac2be3c90c2881a
jaehyunjo <joh1160@naver.com>
2021-08-02 13:49:22 +0900

    테스트

--- src/com/gg/user/dao/PointDAO.java
+++ src/com/gg/user/dao/PointDAO.java
@@ -81,8 +81,6 @@ public class PointDAO {
 
 		return success;
 	}
-
-<<<<<<< HEAD
 	public ArrayList<GGDto> pointList(String id) throws SQLException {
 		System.out.println("DAO 진입");
 		sql = "";
@@ -94,12 +92,7 @@ public class PointDAO {
 			GGDto dto = new GGDto();
 			//dto.set
 		}
-		
-=======
-	public GGDto pointList(String id) {
-		sql="";
->>>>>>> 86a307f30940c84a8de5a4bef57e3a09263a2d7d
-		
+	
 		return null;
 	}
 
