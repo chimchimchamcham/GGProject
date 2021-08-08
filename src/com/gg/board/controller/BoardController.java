0f6a6666b25e71b36585c6545d8a3c0d7e09deff
hyesuelee <lhs75391@naver.com>
2021-08-08 17:45:58 +0900

    중요알람 읽은 항목도 불러옴

--- src/com/gg/board/controller/BoardController.java
+++ src/com/gg/board/controller/BoardController.java
@@ -219,7 +219,7 @@ public class BoardController extends HttpServlet {
 			
 			System.out.println("rqnoval:"+rqnoval);
 			System.out.println("rqnotext:"+rqnotext);
-			service.updatereqlist(rqnoval);
+			//service.updatereqlist(rqnoval);
 			break;
 			
 		case "/lovelist":
