9edd441c85cde18950bce9e0899e5a2f895a4a1d
BongJoSong <kokobor55@naver.com>
2021-08-09 18:11:49 +0900

    신고 수정 완료

--- src/com/gg/user/dao/BlackListDAO.java
+++ src/com/gg/user/dao/BlackListDAO.java
@@ -28,14 +28,24 @@ public class BlackListDAO {
 		boolean success = false;
 		System.out.println(dto.getN_sendId());
 		try {
-			ps = conn.prepareStatement(sql);
+			ps = conn.prepareStatement(sql, new String[] { "n_no" });
 			ps.setString(1, dto.getN_receiveId());
 			ps.setString(2, dto.getN_sendId());
 			ps.setString(3, dto.getN_content());
 			ps.setString(4, dto.getN1_code());
 			ps.setString(5, dto.getN2_code());
+			ps.executeUpdate();
+			rs = ps.getGeneratedKeys();
 			if(ps.executeUpdate()>0) {
-				success = true;
+				if (rs.next()) {
+					sql = "INSERT INTO his_notify(n_no,HN_tm,HN_code) VALUES(?,SYSDATE,'HN_001')";
+					ps = conn.prepareStatement(sql);
+					int pk = rs.getInt(1);
+					ps.setInt(1, pk);
+					if(ps.executeUpdate()>0) {
+						success = true;
+					}
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
