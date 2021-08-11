d654f36c065a7f73362bf3e0f7a8bb635a4ce37c
jaehyunjo <joh1160@naver.com>
2021-08-12 00:10:24 +0900

    글쓰기 커뮤니티 오류 수정

--- src/com/gg/board/controller/BoardController.java
+++ src/com/gg/board/controller/BoardController.java
@@ -247,6 +247,7 @@ public class BoardController extends HttpServlet {
 			service.flow_list(userid, flowORflowing);
 			
 			break;
+			
 		case "/flowadddelect":
 			System.out.println("팔로워 버튼 처리 요청");
 			req.setCharacterEncoding("utf-8");
